7789b62b219effbdaf7fed9a63ac31d2876919a3
Akash Pandit <34327539+akash-pandit@users.noreply.github.com>
2024-02-12 19:41:30 -0800

    Finished lab 1

--- Lab1/Account.java
+++ Lab1/Account.java
@@ -23,39 +23,55 @@ public class Account
 
 	public void displayBalance()
 	{
-		String output = "The balance is $%.2f";
+		String output = "The balance is $%,.2f\n";
 		output = String.format(output, balance);
 		System.out.println(output);
 	}
 	
 	public void displayRate() {
-		String output = "The rate is $%.2f";
-		output = String.format(output, rate);
+		String output = "The interest rate is %f percent\n";
+		output = String.format(output, rate * 100);
 		System.out.println(output);
 	}
 	
 	public boolean deposit(double amount) {
 		if (amount < 0) {
-			throw new BalanceError("You cannot deposit a negative amount of money.");
+			System.out.println("You cannot deposit a negative amount of money. " + 
+			"Please input a valid amount, or 0 to cancel this transaction.\n");
+			return false;
 		}
 		balance += amount;
 		return true;
 	}
 	
 	public boolean withdraw(double amount) {
+		if (amount < 0) {
+			System.out.println("You cannot withdraw a negative amount of money. "  +
+			"To deposit a balance, please use the intended deposit function.\n");
+		}
 		if (amount > balance) {
-			throw new BalanceError("You cannot withdraw more money than is in your account.");
+			System.out.println("You cannot withdraw more money than is in your account. " +
+			"Please input a valid amount, or 0 to cancel this transaction.\n");
+			return false;
 		}
 		balance -= amount;
 		return true;
 	}
 	 
 	public boolean accrueInterest(int months) {
+		if (months < 0) {
+			System.out.println("You cannot track interest with a negative months value. " +
+			"Please input a valid amount, or 0 to not accrue any interest.");
+			return false;
+		}
+		double interestAccrued = 0;
 		for (int i = 0; i < months; i++) {
+			interestAccrued += (double) (balance * rate) / 12;
 			balance = ((double) (balance * rate) / 12) + balance;
 		}
+
+		System.out.println(String.format("Accrued $%,.2f interest over %,d months.\n", interestAccrued, months));
+
 		return true;
 	}
 }
-
-public class BalanceError extends Error {}
--- Lab1/BankAccount.java
+++ Lab1/BankAccount.java
@@ -13,13 +13,12 @@ public class BankAccount {
 	{
 		
 		Account acct = new Account();
-		
 		int choice=99;
-
+		double amount; // placeholder for deposit/withdrawal amt
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Welcome to Town Bank\n");
-		
+
 		while(choice != 6)
 		{
 			choice = menu();
@@ -28,20 +27,57 @@ public class BankAccount {
 				case 1: 
 					acct.displayBalance();
 					break;
+
 				case 2:
 					acct.displayRate();
 					break;
-				case 3: 
-					System.out.println("TODO: Deposit Money\n\n");
+
+				case 3:
+					System.out.print("How much would you like to deposit? ");
+					try {  // catch non-double type deposits
+						amount = sc.nextDouble();
+						if (!acct.deposit(amount)) {  // catch negative deposits
+							amount = sc.nextDouble();
+							acct.deposit(amount);
+						}
+						System.out.println(String.format("$%,.2f has been deposited in your account.\n", amount));  // success
+					} catch (java.util.InputMismatchException e) {
+						System.out.println("Please input a valid amount.\n");
+						sc.next();
+						choice=99;
+					}
 					break;
+
 				case 4:  
-					System.out.println("TODO: Withdraw Money\n\n");
+					System.out.print("How much would you like to withdraw? ");
+					try {  // catch non-double type withdrawals
+						amount = sc.nextDouble();
+						if (!acct.withdraw(amount)) {  // catch negative/greater than balance withdrawals
+							amount = sc.nextDouble();
+							acct.withdraw(amount);
+						}
+						System.out.println(String.format("$%,.2f has been withdrawn in your account.\n", amount));  // success
+					} catch (java.util.InputMismatchException e) {
+						System.out.println("!!Non Integer entered!!");
+						sc.next();
+						choice=99;
+					}
 					break;
 				case 5:  
-					System.out.println("TODO: Calculate Accrual\n\n");
+					System.out.print("How many months would you like to project your interest for? ");
+					int months;
+					try {
+						months = sc.nextInt();
+						acct.accrueInterest(months);
+					} catch (java.util.InputMismatchException e) {
+						System.out.println("!!Non Integer entered!!");
+						sc.next();
+						choice=99;
+					}
 					break;
 				case 6: 
 					System.out.println("!!Thank you and Good Bye!!\n");
+					sc.close();
 					break;
 			}
 		}
@@ -83,6 +119,4 @@ public class BankAccount {
 		System.out.println(); //prints blank line
 		return choice;
 	}
-
-
 }
