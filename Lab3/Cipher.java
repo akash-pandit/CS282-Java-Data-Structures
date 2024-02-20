e89156454f0ca2acaa5a7300870ea1a5ccfe85b7
Akash Pandit <34327539+akash-pandit@users.noreply.github.com>
2024-02-20 00:05:32 -0800

    todo: should translation keep case sensitivity

---
+++ Lab3/Cipher.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.ArrayDeque;
+
+public class Cipher {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Give a keyword for the cipher: ");
+        String keyword = scan.nextLine();
+        CipherList cipher = new CipherList(keyword);
+        cipher.setCipher(scan.nextLine());
+    }
+}
---
+++ Lab3/CipherList.java
@@ -0,0 +1,56 @@
+import java.util.ArrayDeque;
+import java.util.HashMap;
+
+public class CipherList {
+    private char[] alphabetArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    private ArrayDeque<Character> shiftedStack = new ArrayDeque<Character>();
+    private HashMap<Character, Integer> charToInt = new HashMap(26);
+
+    public CipherList(String keyword) {
+        populateMap();
+        setCipher(keyword);
+        System.err.println(shiftedStack);
+    }  // end constructor
+
+    public void setCipher(String keyword) {
+        keyword = getUniqueKeyword(keyword.toUpperCase());
+
+        // add keyword chars to front of shifted stack
+        for (char c : keyword.toCharArray()) {
+            shiftedStack.addLast(c);
+        }
+
+        // add the remaining chars
+        for (char c : alphabetArr) {
+            if (!shiftedStack.contains(c)) {
+                shiftedStack.addLast(c);
+            }
+        }
+    }
+
+    /**
+     * Encrypts a string of length 140 with the instance cipher.
+     * @return encrypted string
+     */
+    public String encrypt(String input) {
+        // for (char c : input.toCharArray()) {
+        //     int alphabetPos = c - 'A'
+        // }
+        return "";
+    }
+
+    private String getUniqueKeyword(String keyword) {
+        String output = "";
+        for (char c : keyword.toCharArray()) {
+            if (output.indexOf(c) == -1) {
+                output += c;
+            }
+        }
+        return output;
+    }
+
+    private void populateMap() {
+
+    }
+
+}  // end class
