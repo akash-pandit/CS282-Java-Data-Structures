262bddafb5051f174656b04e3869779a69df1ef7
akash-pandit <34327539+akash-pandit@users.noreply.github.com>
2024-05-17 13:26:27 -0700

    added comparison & case insensitiviity, changed tostring

--- Lab6/Contact.java
+++ Lab6/Contact.java
@@ -5,13 +5,26 @@ public class Contact {
     public String address;
 
     Contact(String firstName, String lastName, int phoneNumber, String address) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.firstName = firstName.toLowerCase();
+        this.lastName = lastName.toLowerCase();
         this.phoneNumber = phoneNumber;
         this.address = address;
     }
 
+    public boolean greaterThan(Contact contact) {
+        if (this.lastName.compareTo(contact.lastName) > 0)
+            // this lastname greater, should go after contact
+            return true;
+        
+        if (this.lastName.equals(contact.lastName)) {
+            // last names match, comparing first names
+            if (this.firstName.compareTo(contact.firstName) > 0)
+                return true;
+        }
+        return false;
+    }
+
     public String toString() {
-        return firstName;
+        return String.format("%s, %s (%d)", lastName, firstName, phoneNumber);
     }
 }
