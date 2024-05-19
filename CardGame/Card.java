e651577c97354d7b0c189bccf76a08eaf4ea8722
Akash Pandit <34327539+akash-pandit@users.noreply.github.com>
2024-05-18 23:51:08 -0700

    started final proj

---
+++ CardGame/Card.java
@@ -0,0 +1,8 @@
+public class Card {
+    private int rank;
+    private char suit;
+
+    public Card(int rank, char suit) {
+        
+    }
+}
---
+++ CardGame/Deck.java
@@ -0,0 +1,13 @@
+public class Deck {
+    private Card[] deck;
+    private char[] suits = {'d', 'c', 'h', 's'};
+
+    public Deck() {
+        deck = new Card[52];
+
+
+        for (int i = 1; i < 53; i++) {
+            
+        }
+    }
+}
